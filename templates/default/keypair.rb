36f088249052d7ad3ca11407144407790c89e2d4
Jay Zeschin <jay.zeschin@factorylabs.com>
2010-12-27 14:24:36 -0700

    Force the keypair step to use a blank password

--- templates/default/keypair.rb
+++ templates/default/keypair.rb
@@ -1,4 +1,4 @@
 # Build a keypair
 log "\n    Creating a repo keypair ...\n"
 empty_directory 'config/keypair'
-run "/usr/bin/ssh-keygen -q -t rsa -f config/keypair/id_rsa"
+run 'echo "\n\n" | /usr/bin/ssh-keygen -q -t rsa -f config/keypair/id_rsa'
