6b7845b590d3a4b99be4cd8301b844bc02cbda68
Jay Zeschin <jay.zeschin@factorylabs.com>
2010-12-27 13:55:36 -0700

    Removing duplicate steps in public template

--- templates/default/public.rb
+++ templates/default/public.rb
@@ -1,13 +1,6 @@
 # Tweak the Public directory
 say "\n-- Processing public directory ...\n"
 
-# Remove files we don't care about
-say "\n    Removing unnecessary files ...\n"
-remove_file "public/index.html"
-remove_file "public/favicon.ico"
-remove_file "public/robots.txt"
-remove_file "public/images/rails.png"
-
 # Create a sane robots.txt
 log "\n    Creating a default robots.txt ...\n"
 robots_txt = <<-ROBOTS.gsub(/^ {2}/, '')
